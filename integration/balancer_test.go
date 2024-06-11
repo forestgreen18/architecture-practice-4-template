ff3b1cef4e709d254e4af5d5c414f26e0e47511f
Illia Kornyk <forest.bukson@gmail.com>
2024-06-11 23:48:30 +0300

    test: balancer logic

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -4,29 +4,67 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 	"testing"
-	"time"
+
+	"gopkg.in/check.v1"
 )
 
 const baseAddress = "http://balancer:8090"
 
-var client = http.Client{
-	Timeout: 3 * time.Second,
-}
+var _ = check.Suite(&BalancerSuite{})
 
-func TestBalancer(t *testing.T) {
+type BalancerSuite struct{}
+
+func Test(t *testing.T) { check.TestingT(t) }
+
+func (s *BalancerSuite) TestBalancerLogic(c *check.C) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+		c.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	var totalRequests = 100
+	var serverResponses = make(map[string]int)
+	var mu sync.Mutex
+
+	// Send multiple requests to the balancer and record the server each response comes from.
+	for i := 0; i < totalRequests; i++ {
+		resp, err := http.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		c.Assert(err, check.IsNil)
+		defer resp.Body.Close()
+
+		// Check if the response status code is 200 OK.
+		c.Assert(resp.StatusCode, check.Equals, http.StatusOK)
+
+		// Record the server that responded.
+		server := resp.Header.Get("lb-from")
+		c.Assert(server, check.Not(check.Equals), "")
+
+		mu.Lock()
+		serverResponses[server]++
+		mu.Unlock()
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	// Log the number of responses from each server.
+	for server, count := range serverResponses {
+		c.Logf("Server %s responded to %d requests", server, count)
+	}
+
+	// Check if the distribution of requests is as expected.
+	// The server with the least traffic should have responded to the most requests.
+	var maxResponses int
+	for _, count := range serverResponses {
+		if count > maxResponses {
+			maxResponses = count
+		}
+	}
+	for server, count := range serverResponses {
+		// Use a boolean expression to check the condition.
+		c.Assert(count <= maxResponses, check.Equals, true, check.Commentf("Server %s responded to too many requests", server))
+	}
 }
+
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+
 }
